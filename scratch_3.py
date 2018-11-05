90f154682dee21bafcfd76d4a99239875b12d728
rorymulcahey <38397407+rorymulcahey@users.noreply.github.com>
2018-11-05 12:42:30 -0800

    Update scratch_3.py

--- scratch_3.py
+++ scratch_3.py
@@ -6,29 +6,5 @@ What is the largest prime factor of the number 600851475143 ?
 
 '''
 
-import math
-# initial = 13195
-# h_prime = 1
-initial = 600851475143
-
-
-def find_factor(number):
-    current_prime = 1
-    for x in range(1, round(math.sqrt(number))):
-        if number % x == 0:
-            if check_if_prime(x):
-                current_prime = x
-    return current_prime
-
-
-def check_if_prime(h_factor):
-    for y in range(2, h_factor):
-        if h_factor % y == 0:
-            return False
-    return True
-
-
-h_prime = find_factor(initial)
-print(h_prime)
-
+solved
 
